61998ac3119d4bb4cec31fd2357c373762228dd0
aniketpowar0007 <54701556+aniketpowar0007@users.noreply.github.com>
2020-06-14 18:52:22 +1000

    Add files via upload

---
+++ build/precache-manifest.f59314f1155b855ddd9986872e4b4c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebdc655961199b84511cfd9d3bd3fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c53e078529c27491738e",
+    "url": "/static/css/2.55e17ba8.chunk.css"
+  },
+  {
+    "revision": "7282abe280ca1af621b5",
+    "url": "/static/css/main.3bb268b6.chunk.css"
+  },
+  {
+    "revision": "c53e078529c27491738e",
+    "url": "/static/js/2.375fe15a.chunk.js"
+  },
+  {
+    "revision": "f0a2d918315133595be1d53a43083e6b",
+    "url": "/static/js/2.375fe15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7282abe280ca1af621b5",
+    "url": "/static/js/main.2d20d793.chunk.js"
+  },
+  {
+    "revision": "e661affb54845a79021d",
+    "url": "/static/js/runtime-main.d16d2295.js"
+  }
+]);
